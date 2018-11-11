2d01c8f035294029f5d5d114d1369c93d0a9149f
mariobermudezjr <mbermudez91@gmail.com>
2018-11-11 12:59:26 -0800

    feature: embed a video within an activity

---
+++ app/src/main/java/com/mariobermudezjr/challengeassignment/VideoActivity.java
@@ -0,0 +1,26 @@
+package com.mariobermudezjr.challengeassignment;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        VideoView videoView =(VideoView)findViewById(R.id.videoView);
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+
+        videoView.start();
+    }
+}
